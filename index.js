ace3a23ccb17d5100064c4089c5e4db7f976331b
yabhi1205 <83153764+yabhi1205@users.noreply.github.com>
2023-09-05 09:16:32 +0530

    demo

--- index.js
+++ index.js
@@ -2,18 +2,12 @@ const express = require('express');
 var cors = require('cors');
 const connectToMongo = require('./db');
 connectToMongo()
-// connect();
 const app = express()
 const port = 5000
 app.use(cors())
 app.use(express.json())
 // Available Routes
 app.use('/',require('./routes/add'))
-// app.use('/api/admin',require('./routes/admin'))
-// app.use('/api/manager',require('./routes/manager'))
-// app.use('/api/books',require('./routes/books'))
-// app.use('/api/search',require('./routes/search'))
-// app.use('/api/otp',require('./routes/otp'))
 app.listen(port,()=>{
     console.log(`We are listening at the localhost:${port}`);
 })
